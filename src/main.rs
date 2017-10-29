d86703d24f4fa424a29bbe2dac122efb43f478e5
Alexander Serebryakov <alex.serebr@gmail.com>
2017-10-29 18:08:08 +0200

    Full duplex between server and client

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,6 @@ use std::sync::mpsc::Sender;
 use std::sync::mpsc::Receiver;
 
 
-struct Client {
-    nickname : String,
-    stream : TcpStream,
-    thread_handle : thread::JoinHandle<()>,
-}
-
-
 fn read_line(mut stream: &TcpStream) -> Result<String, std::io::Error> {
     let mut line = String::new();
     let mut end = false;
@@ -31,7 +24,7 @@ fn read_line(mut stream: &TcpStream) -> Result<String, std::io::Error> {
 }
 
 
-fn handle_client(mut stream: TcpStream) -> Result<Sender<String>, std::io::Error> {
+fn handle_client(mut stream: TcpStream, server_tx : Sender<String>) -> Result<Sender<String>, std::io::Error> {
     let builder = thread::Builder::new();
     let (sender , receiver) : (Sender<String>, Receiver<String>)  = channel();
 
@@ -39,10 +32,11 @@ fn handle_client(mut stream: TcpStream) -> Result<Sender<String>, std::io::Error
         let _ = stream.write("Greetings!\n\0".as_bytes()).unwrap();
         let _ = stream.write("Please enter your nickname: ".as_bytes()).unwrap();
 
-        println!("Clients nickname is {}", read_line(&stream).unwrap());
+        let nickname = read_line(&stream).unwrap();
 
-        let _ = stream.write(&receiver.recv().unwrap().into_bytes());
-    });
+        let _ = server_tx.send(nickname.clone());
+        let _ = stream.write(&receiver.recv().unwrap().into_bytes()).unwrap();
+    }).unwrap();
 
     Ok(sender)
 }
@@ -52,12 +46,17 @@ fn main() {
     println!("Initializing...");
 
     let listener = TcpListener::bind("127.0.0.1:40000").unwrap();
+    let (tx, rx) : (Sender<String>, Receiver<String>)  = channel();
 
     println!("Waiting for clients...");
 
     for stream in listener.incoming() {
         println!("Client is connected...");
-        let sender = handle_client(stream.unwrap()).unwrap();
-        sender.send(String::from("Greetings From Server!\n"));
+
+        let client_tx = handle_client(stream.unwrap(), tx.clone()).unwrap();
+        let nickname = rx.recv().unwrap();
+
+        println!("Clients nickname is {}", nickname);
+        client_tx.send(String::from("Greetings From Server!\n")).unwrap();
     }
 }
