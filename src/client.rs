17894359cc78343df555fb50567dc9d0232d74d6
Alexander Serebryakov <alex.serebr@gmail.com>
2017-12-06 17:51:27 +0200

    Implements the matching nicknames handling

--- src/client.rs
+++ src/client.rs
@@ -6,7 +6,7 @@ use std::thread::Builder;
 use std::time::Duration;
 use std::clone::Clone;
 use utils;
-use server::ServerMessage;
+use server::{ServerMessage, Reason};
 
 
 pub struct ClientInfo {
@@ -38,45 +38,104 @@ pub struct Client {
 
 
 impl Client {
-    pub fn new(
-        mut stream: TcpStream,
-        server_tx: Sender<ClientMessage>,
-    ) -> Result<(), std::io::Error> {
+    pub fn new(stream: TcpStream, server_tx: Sender<ClientMessage>) -> Result<(), std::io::Error> {
         let (tx, rx): (Sender<ServerMessage>, Receiver<ServerMessage>) = channel();
 
         let builder = Builder::new();
-        builder.spawn(move || {
-            if let Err(e) = stream.write("Greetings!\nPlease enter your nickname: ".as_bytes()) {
-                eprintln!("Failed to request the clients nickname : {:?}", e);
-                ()
-            }
-
-            let nickname = utils::read_line(&stream).unwrap();
-
-            let info = ClientInfo { nickname, tx };
-
-            let _ = server_tx
-                .send(ClientMessage::TryConnect(info.clone()))
-                .unwrap();
-
-            if let Err(e) = Client::main_loop(
-                Client { info: info.clone() },
-                stream,
-                &server_tx,
-                rx,
-            )
-            {
-                eprintln!("Client exit with error {:?}", e);
-                if let Err(e) = server_tx.send(ClientMessage::Disconnect(String::from(
-                    format!("{} disconnected", info.nickname.clone()),
-                )))
+        builder.spawn(move || match Client::try_to_connect(
+            &stream,
+            &server_tx,
+            tx,
+            &rx,
+        ) {
+            Ok(info) => {
+                if let Err(e) = Client::main_loop(
+                    Client { info: info.clone() },
+                    stream,
+                    &server_tx,
+                    rx,
+                )
                 {
-                    eprintln!("Couldn't send the disconnect {:?}", e);
+                    eprintln!("Client exit with error {:?}", e);
+                    if let Err(e) = server_tx.send(ClientMessage::Disconnect(String::from(
+                        format!("{} disconnected", info.nickname.clone()),
+                    )))
+                    {
+                        eprintln!("Couldn't send the disconnect {:?}", e);
+                    }
                 }
+            }
+            Err(e) => {
+                eprintln!("Client connection error {:?}", e);
             }
         })?;
 
         Ok(())
+    }
+
+
+    fn try_to_connect(
+        mut stream: &TcpStream,
+        server_tx: &Sender<ClientMessage>,
+        client_tx: Sender<ServerMessage>,
+        client_rx: &Receiver<ServerMessage>,
+    ) -> Result<ClientInfo, std::io::Error> {
+        loop {
+            stream.write(
+                "Greetings!\nPlease enter your nickname: ".as_bytes(),
+            )?;
+
+            let nickname = utils::read_line(&stream).unwrap();
+            let info = ClientInfo {
+                nickname,
+                tx: client_tx.clone(),
+            };
+
+            if let Err(e) = server_tx.send(ClientMessage::TryConnect(info.clone())) {
+                eprintln!("Couldn't send the message {:?}", e);
+
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Server connection error",
+                ));
+            }
+
+            match client_rx.recv() {
+                Ok(msg) => {
+                    match msg {
+                        ServerMessage::ConnectOk => return Ok(info),
+                        ServerMessage::ConnectError(e) => {
+                            match e {
+                                Reason::NicknameAlreadyUsed => {
+                                    stream.write("Nickname already used. Try again.\n".as_bytes())?;
+                                }
+                                Reason::GeneralError => {
+                                    stream.write(
+                                        "Server error, you will be disconnected.\n".as_bytes(),
+                                    )?;
+                                    return Err(std::io::Error::new(
+                                        std::io::ErrorKind::Other,
+                                        "General Server Error",
+                                    ));
+                                }
+                            }
+                        }
+                        _ => {
+                            return Err(std::io::Error::new(
+                                std::io::ErrorKind::Other,
+                                "Unexpected message received",
+                            ))
+                        }
+                    }
+                }
+                Err(_) => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        "Couldn't receive message from server",
+                    ))
+                }
+            }
+        }
     }
 
 
